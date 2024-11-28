8ccf7823b2f8e06748bd38b904691d8968be3507
BeatrizBaltazar15 <bebaltazar891@gmail.com>
2024-11-28 17:36:12 -0400

    back

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { inf, buscarPorId, infPorAno, reajuste } from "./serviços/serviços.js";
+import { inf, buscarPorId, infPorAno, reajuste } from "./serviço/serviço.js";
 
 const app = express();
 
